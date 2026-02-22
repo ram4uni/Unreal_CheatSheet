818668c2d4fe6fbc7c053b175d9c712da7ce72ae
ram4uni <ram4uni@gmail.com>
2026-02-22 23:20:34 +0000

    EIS added

---
+++ EIS_Guide.cpp
@@ -0,0 +1,73 @@
+void AMovingBox::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (APlayerController* PC = GetWorld()->GetFirstPlayerController())
+	{
+		if (ULocalPlayer* LocalPlayer = PC->GetLocalPlayer())
+		{
+			if (UEnhancedInputLocalPlayerSubsystem* Subsystem =
+				LocalPlayer->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>())
+			{
+				Subsystem->AddMappingContext(MappingContext, 0);
+			}
+		}
+	
+		if (UEnhancedInputComponent* EIC =
+			Cast<UEnhancedInputComponent>(PC->InputComponent)) {
+
+			if (JumpAction)
+			{
+				EIC->BindAction(
+					JumpAction,
+					ETriggerEvent::Started,
+					this,
+					&AMovingBox::Jump
+				);
+			}
+		}
+        
+        	if (UEnhancedInputComponent* EIC =
+		Cast<UEnhancedInputComponent>(PC->InputComponent))
+	{
+		if (MoveAction)
+		{
+			EIC->BindAction(
+				MoveAction,
+				ETriggerEvent::Triggered,
+				this,
+				&AMovingBox::Move
+			);
+		}
+	}
+
+	}	
+}
+
+void AMovingBox::Jump() {
+	if (!BoxMesh) return;
+
+	// Prevent jumping in mid-air (simple ground check)
+	if (BoxMesh->GetPhysicsLinearVelocity().Z == 0.f)
+	{
+		BoxMesh->AddImpulse(
+			FVector(0.f, 0.f, JumpImpulse),
+			NAME_None,
+			true
+		);
+	}
+}
+
+void AMovingBox::Move(const FInputActionValue& Value)
+{
+	const FVector2D Input = Value.Get<FVector2D>();
+
+	FVector DeltaMove = FVector(
+		Input.Y,   // Forward / Back
+		Input.X,   // Right / Left
+		0.f
+	) * MoveSpeed * GetWorld()->GetDeltaSeconds();
+
+	AddActorWorldOffset(DeltaMove, true);
+
+}
